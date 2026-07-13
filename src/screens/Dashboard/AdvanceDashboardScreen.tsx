40eb02ae58b1e798d241a2dae382dc720e0b922c
malikhan4050 <malikhan4050@gmail.com>
2026-07-13 10:55:23 +0500

    removed unnecessary and unused lines from the file

--- src/screens/Dashboard/AdvanceDashboardScreen.tsx
+++ src/screens/Dashboard/AdvanceDashboardScreen.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-import DashboardLogo from '../../components/Logo/DashboardLogo';
+import DashboardLogo from '../../components/Logo/HeaderLogo';
 import styles from './styles';
 
 const AdvanceDashboardScreen = () => {

9d392a783f9f77af21bb4670b7dd61ec259b0ddd
malikhan4050 <malikhan4050@gmail.com>
2026-07-14 14:48:01 +0500

    new styles file created for products screen

---
+++ src/screens/Products/styles.ts
@@ -0,0 +1,39 @@
+import { StyleSheet } from 'react-native';
+
+import Colors from '../../theme/Colors';
+import Sizes from '../../theme/Sizes';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F8F9FA',
+    padding: Sizes.screenPadding,
+  },
+
+  scrollContainer: {
+    flexGrow: 1,
+    paddingBottom: 20,
+  },
+
+  header: {
+    marginTop: 10,
+    marginBottom: 16,
+  },
+
+  headerTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: Colors.primaryText,
+  },
+
+  headerSubtitle: {
+    fontSize: 14,
+    color: Colors.secondaryText,
+    marginTop: 2,
+  },
+  subtitle: {
+    fontSize: 14,
+    color: Colors.secondaryText,
+    marginTop: 4,
+  },
+});

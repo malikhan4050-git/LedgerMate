e9be1a5b63ca09008f5707c90b9efcf82a11c764
malikhan4050 <malikhan4050@gmail.com>
2026-07-14 14:44:30 +0500

    added enough colors for more screens

--- src/theme/Colors.ts
+++ src/theme/Colors.ts
@@ -3,6 +3,7 @@ const Colors = {
   primaryGreen: '#4CCB8C',
 
   primaryText: '#1F4265',
+  
   secondaryText: '#6D6D6D',
 
   white: '#FFFFFF',
@@ -16,8 +17,16 @@ const Colors = {
   divider: '#D9D9D9',
 
   google: '#DB4437',
+
   facebook: '#1877F2',
+  
   apple: '#000000',
+  
+  primary: '#ffffff',
+  
+  WHITE: '#ffffff',
+  
+  BLACK: '#000000',
 };
 
 export default Colors;

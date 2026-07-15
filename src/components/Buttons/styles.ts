1f765bac5e6d46f582bd7543e4e0cbee70509ce9
malikhan4050 <malikhan4050@gmail.com>
2026-07-15 09:28:22 +0500

    removed unnecessary styles of the buttons

--- src/components/Buttons/styles.ts
+++ src/components/Buttons/styles.ts
@@ -42,57 +42,6 @@ export default StyleSheet.create({
     borderRadius: Sizes.borderRadius,
     backgroundColor: 'rgba(0,0,0,0.06)',
   },
-
-  socialButton: {
-    height: 58,
-
-    borderRadius: Sizes.borderRadius,
-
-    backgroundColor: Colors.white,
-
-    flexDirection: 'row',
-
-    alignItems: 'center',
-
-    justifyContent: 'center',
-
-    marginBottom: 16,
-
-    borderWidth: 1,
-
-    borderColor: '#ECECEC',
-
-    shadowColor: '#000',
-
-    shadowOpacity: 0.12,
-
-    shadowRadius: 7,
-
-    shadowOffset: {
-      width: 0,
-      height: 4,
-    },
-
-    elevation: 6,
-  },
-
-  icon: {
-    width: 28,
-
-    height: 28,
-
-    resizeMode: 'contain',
-
-    marginRight: 14,
-  },
-
-  socialText: {
-    fontSize: 18,
-
-    fontWeight: '600',
-
-    color: '#000',
-  },
   disabledButton: {
     opacity: 0.7,
   },

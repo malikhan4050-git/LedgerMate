d3afed165656caa5f6516eea0a1607f306e7c60c
malikhan4050 <malikhan4050@gmail.com>
2026-07-20 09:50:11 +0500

    ip changed network changed

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.15:8000/api',
+  baseURL: 'http://10.45.14.158:8000/api',
   headers: {
     'Content-Type': 'application/json',
   },

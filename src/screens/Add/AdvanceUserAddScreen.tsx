6723e09e50eaaed9a6e93dc86d696542fac82007
malikhan4050 <malikhan4050@gmail.com>
2026-07-20 12:30:38 +0500

    simple code inserted until now for the testing purposes....

---
+++ src/screens/Add/AdvanceUserAddScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const AdvanceUserAddScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>This is Advance User's Add Screen</Text>
+    </View>
+  );
+};
+
+export default AdvanceUserAddScreen;

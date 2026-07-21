d5d301a1de3f3e262849f0796dd024eaeb9c93a5
malikhan4050 <malikhan4050@gmail.com>
2026-07-21 18:36:30 +0500

    added the wrapper alert provider around the whole code...

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
+import { AlertProvider } from './src/components/CustomAlert/AlertProvider';
 import RootNavigator from './src/navigation/RootNavigator';
 import { store } from './src/redux/store';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <RootNavigator />
+      <AlertProvider>
+        <RootNavigator />
+      </AlertProvider>
     </Provider>
   );
 };

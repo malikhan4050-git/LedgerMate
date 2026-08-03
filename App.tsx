daa0ee4792e866da07b5140e1fa57bd8e1f67c9e
malikhan4050 <malikhan4050@gmail.com>
2026-08-03 12:32:38 +0500

    implemented deep linking..

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+// App.tsx
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
-
+import { Linking } from 'react-native';
 import { AlertProvider } from './src/components/CustomAlert/AlertProvider';
 import RootNavigator from './src/navigation/RootNavigator';
 import { store } from './src/redux/store';
 
 const App = () => {
+  // Optional: Handle deep link when app is closed
+  useEffect(() => {
+    const getInitialURL = async () => {
+      const url = await Linking.getInitialURL();
+      if (url) {
+        console.log('Initial URL:', url);
+      }
+    };
+    getInitialURL();
+  }, []);
+
   return (
     <Provider store={store}>
       <AlertProvider>
